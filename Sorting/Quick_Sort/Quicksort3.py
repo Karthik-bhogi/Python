6fd58532dd271cb8ae99a5aa35c632ad55e4e400
Karthik Bhogi <karthikbhogi1108@gmail.com>
2021-08-25 22:02:40 +0530

    Add files via upload

---
+++ Sorting/Quick_Sort/Quicksort3.py
@@ -0,0 +1,71 @@
+'''
+Created on Aug 22, 2021
+
+@author: Karthik
+'''
+# to get the correct position of the pivot element
+#we have taken random element as pivot
+# Ascending order
+import random
+def pivot_place1(list1,first,last):
+    rindex = random.randint(first,last)
+    list1[rindex],list1[last] = list1[last],list1[rindex]
+    pivot = list1[last]
+    left = first
+    right = last-1
+    while True :
+        while left<=right and list1[left] <= pivot :
+            left = left + 1
+        while left<=right and list1[right] >= pivot :
+            right = right - 1
+        if right < left :
+            list1[last],list1[left] = list1[left],list1[last]
+            break
+        else :
+            list1[left],list1[right] = list1[right],list1[left]
+    return left
+
+#descending order
+def pivot_place2(list1,first,last):
+    pivot = list1[last]
+    left = first
+    right = last-1
+    while True :
+        while left<=right and list1[left] >= pivot :
+            left = left + 1
+        while left<=right and list1[right] <= pivot :
+            right = right - 1
+        if right < left :
+            list1[last],list1[left] = list1[left],list1[last]
+            break
+        else :
+            list1[left],list1[right] = list1[right],list1[left]
+    return left
+
+def quicksort1(list1,first,last):
+    if first < last :
+        p1 = pivot_place1(list1, first, last) 
+        quicksort1(list1,first,p1-1)
+        quicksort1(list1,p1+1,last)
+
+def quicksort2(list1,first,last):
+    if first < last :
+        p2 = pivot_place2(list1, first, last) 
+        quicksort2(list1,first,p2-1)
+        quicksort2(list1,p2+1,last)
+        
+#main
+num = int(input("How many numbers you want are there in the list ? :"))
+list1 = []
+for k in range(num):
+    list1.append(int(input("Enter Number :")))
+# list1 = [56,26,93,17,31,44,17]
+print("Given list :",list1)
+n = len(list1)
+quicksort1(list1,0,n-1)
+print("Ascending Order :",list1)
+quicksort2(list1,0,n-1)
+print("Descending Order :",list1)
+
+
+    
---
+++ Sorting/Quick_Sort/Quicksort4.py
@@ -0,0 +1,77 @@
+'''
+Created on Aug 22, 2021
+
+@author: Karthik
+'''
+# to get the correct position of the pivot element
+#we have taken median element as pivot
+# Ascending order
+import statistics
+def pivot_place1(list1,first,last):
+    low = list1[first]
+    high = list1[last]
+    mid = (first + last)//2
+    pivot_val = statistics.median([low,list1[mid],high])
+    if pivot_val == low :
+        pindex = first
+    elif pivot_val == high :
+        pindex = last
+    else: 
+        pindex = mid
+    list1[last],list1[pindex] = list1[pindex],list1[last] 
+    pivot = list1[last]        
+    left = first
+    right = last-1
+    while True :
+        while left<=right and list1[left] <= pivot :
+            left = left + 1
+        while left<=right and list1[right] >= pivot :
+            right = right - 1
+        if right < left :
+            list1[last],list1[left] = list1[left],list1[last]
+            break
+        else :
+            list1[left],list1[right] = list1[right],list1[left]
+    return left
+
+#descending order
+def pivot_place2(list1,first,last):
+    pivot = list1[last]
+    left = first
+    right = last-1
+    while True :
+        while left<=right and list1[left] >= pivot :
+            left = left + 1
+        while left<=right and list1[right] <= pivot :
+            right = right - 1
+        if right < left :
+            list1[last],list1[left] = list1[left],list1[last]
+            break
+        else :
+            list1[left],list1[right] = list1[right],list1[left]
+    return left
+
+def quicksort1(list1,first,last):
+    if first < last :
+        p1 = pivot_place1(list1, first, last) 
+        quicksort1(list1,first,p1-1)
+        quicksort1(list1,p1+1,last)
+
+def quicksort2(list1,first,last):
+    if first < last :
+        p2 = pivot_place2(list1, first, last) 
+        quicksort2(list1,first,p2-1)
+        quicksort2(list1,p2+1,last)
+        
+#main
+num = int(input("How many numbers you want are there in the list ? :"))
+list1 = []
+for k in range(num):
+    list1.append(int(input("Enter Number :")))
+# list1 = [56,26,93,17,31,44,17]
+print("Given list :",list1)
+n = len(list1)
+quicksort1(list1,0,n-1)
+print("Ascending Order :",list1)
+quicksort2(list1,0,n-1)
+print("Descending Order :",list1)
