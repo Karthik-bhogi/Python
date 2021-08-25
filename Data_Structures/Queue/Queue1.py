d11b32250499b70e6b0c584d73762a84cb9c1863
Karthik Bhogi <karthikbhogi1108@gmail.com>
2021-08-25 22:07:32 +0530

    Create Queue1.py

---
+++ Data_Structures/Queue/Queue1.py
@@ -0,0 +1,47 @@
+queue = []
+print("Select the Operation \n 1.Enqueue \n 2.Dequeue \n 3.First \n 4.Last \n 5.Quit")
+def enqueue(N):
+    if len(queue)==N :
+        print("queue is full")
+    else :
+        element = input("Enter the Element :")
+        queue.append(element)
+        print(queue)
+
+def dequeue():
+    if not queue or len(queue)==0 : # one condition is enough
+        print("queue is Empty")
+    else:
+        e = queue.pop(0)
+        print("Removed Element :",e)
+        print(queue)
+
+def front(queue):
+    if not queue or len(queue)==0 : # one condition is enough
+        print("queue is Empty")
+    else:
+        print("First Element :",queue[0])
+
+def back(queue):
+    if not queue or len(queue)==0 : # one condition is enough
+        print("queue is Empty")
+    else:
+        print("Last Element :",queue[-1])
+        
+N = int(input("Enter the limit of stack :"))
+while True :
+    print("Choice :",end='')
+    choice = int(input())
+    if choice == 1 :
+        enqueue(N)
+    elif choice == 2 :
+        dequeue()
+    elif choice == 3 :
+        front(queue)
+    elif choice == 4 :
+        back(queue)
+    elif choice == 5 :
+        break
+    else :
+        print("Enter the correct choice")
+print("Program Execution completed")
